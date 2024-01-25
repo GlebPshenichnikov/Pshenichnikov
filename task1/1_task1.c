678185371d66d16ba43e664caf126a51ed267eb8
GlebPshenichnikov <145165619+GlebPshenichnikov@users.noreply.github.com>
2024-01-25 17:24:27 +0300

    Add files via upload

---
+++ task1/1_task1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main(void){
+	printf("Size of short is %ld\n", sizeof(short));
+	printf("Sizendguughgu of int is %ld\n", sizeof(int));
+	printf("Size of long is %ld\n", sizeof(long));
+	printf("Size of float is %ld\n", sizeof(float));
+	printf("Size of double is %ld\n", sizeof(double));
+	printf("Size of long double is %ld\n", sizeof(long double));
+	return 0;
+}
